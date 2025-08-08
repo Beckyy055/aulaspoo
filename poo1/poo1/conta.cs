fdfa37c15866964b7a4dc7f8f21d87628871682a
Rebeca Cheli <168734058+Beckyy055@users.noreply.github.com>
2025-08-08 11:38:38 -0300

    Add files via upload

---
+++ poo1/poo1/conta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo1
+{
+    public class Conta
+    {
+        private double saldo;
+        public double Deposito(double valor)
+        {
+            if (valor > 0)
+            {
+                saldo = valor + saldo;
+            }
+            return saldo;
+        }
+++
+        public double Saque(double valor)
+        {
+            if (valor > 0 && saldo >= valor)
+            {
+                saldo = saldo - valor;
+            }
+            return saldo;
+        }
+
+    }
+}
---
+++ poo1/poo1/personagem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo1
+{
+    public class Personagem
+    {
+        private string nome;
+        private int vida;
+        private int mana;
+        
+    }
+}
