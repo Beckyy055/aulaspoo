808c986348d9979b5fb6e68f24de46dd3ac7276a
Rebeca Cheli <168734058+Beckyy055@users.noreply.github.com>
2025-08-15 11:48:02 -0300

    Add files via upload

---
+++ poo2/poo2/funcionario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo2
+{
+    internal class funcionario
+    {
+        private string cargo;
+        private string setor;
+        private List<string> beneficios = new();
+        private string registro;
+        private float salario;
+
+        public Funcionario(string cargo, string setor, string registro, float salario, List<string> beneficios)
+        {
+            this.cargo = cargo;
+            this.setor = setor;
+            this.registro = registro;
+            this.salario = salario;
+            this.beneficios = beneficios;
+        }
+
+        public string Cargo { get { return cargo; } set { cargo = value; } }
+        public string Setor { get { return setor; } set { setor = value; } }
+        public string Registro { get { return registro; } set { registro = value; } }
+        public float Salario { get { return salario; } set { salario = value; } }
+        public List<string> Beneficios { get { return beneficios; } set { beneficios = value; } }
+    }
+    
+}
+
---
+++ poo2/poo2/pessoa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo2
+{
+    public class Pessoa
+    {
+        private string nome;
+        private int idade;
+        private string cpf;
+        private cor = new Cor;
+        private genero = new Genero;
+
+        public Pessoa(string nome, int idade, string cpf, Cor cor, Genero genero)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.cpf = cpf;
+            this.cor = cor;
+            this.genero = genero;
+        }
+        public string Nome { get { return nome; }, set { nome = value; } }
+        public int Idade { get { return idade; } set { idade = value; } }
+        public string Cpf { get { return cpf; } set { cpf = value; } }
+        public Cor cor { get { return cor; } set { cor = value; } }
+        public Genero genero { get { return genero; } set { genero = value; } }
+
+    }
+
+    public enum Cor
+    {
+        Branco, Negro, Indígena, Pardo, Asiático, Indígena, Outro
+    }
+    public enum Genero
+    {
+        mulher cis, mulher trans, homem cis, homem trans, não-binário, agênero, gênero fluido, outro
+    }
+}
