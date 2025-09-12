f44616d7ece157afc7af8b1f4511b165913e3572
Rebeca Cheli <168734058+Beckyy055@users.noreply.github.com>
2025-09-12 11:37:45 -0300

    Add files via upload

---
+++ poo6/poo6/carro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo6
+{
+    public class Carro
+    {
+        private string placa;
+
+        public string Placa
+        {
+            get => placa;
+            set { placa = value; }
+        }
+
+    }
+}
---
+++ poo6/poo6/estacionamento.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo6
+{
+    public class Estacionamento
+    {
+        private decimal caixa = 0.00m;
+        private int limitedecarros = 10;
+        private List<Carro> carros = new List<Carro>(); 
+
+        public void EntradadeCarro ()
+        {
+            Carro carro = new Carro();
+            Console.WriteLine("Digite a placa do carro: ");
+            carro.Placa = Console.ReadLine();
+            carros.Add(carro);
+        }
+
+        public void SaidadeCarro()
+        {
+            Console.WriteLine("Digite a placa do carro que vai sair: ");
+            var id = Console.ReadLine();
+            
+            
+
+        }
+    }
+}
